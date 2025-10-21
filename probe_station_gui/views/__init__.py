b5a42297a495b90135506d80f3aa3644fdb25642
Artyom Polyanskiy <55665542+Lazemir@users.noreply.github.com>
2025-10-21 13:25:12 +0300

    Add serial terminal window and FluidNC utilities

--- probe_station_gui/views/__init__.py
+++ probe_station_gui/views/__init__.py
@@ -1,5 +1,7 @@
 """View widgets for the probe station GUI."""
 
+from .joystick_window import JoystickWindow
 from .microscope_view import MicroscopeView
+from .serial_terminal_window import SerialTerminalWindow
 
-__all__ = ["MicroscopeView"]
+__all__ = ["JoystickWindow", "MicroscopeView", "SerialTerminalWindow"]
