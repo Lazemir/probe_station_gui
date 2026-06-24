03e0ac2cea01f0bf2b3b882176e0a65250d5ad07
Артём Полянский <lazemir@yandex.ru>
2026-06-25 00:32:30 +0300

    Route MoveVector lazy export to stage types

--- probe_station_gui/__init__.py
+++ probe_station_gui/__init__.py
@@ -25,7 +25,7 @@ _LAZY_EXPORTS = {
         "probe_station_gui.views.serial_terminal_window",
         "SerialTerminalWindow",
     ),
-    "MoveVector": ("probe_station_gui.stage_controller", "MoveVector"),
+    "MoveVector": ("probe_station_gui.stage_types", "MoveVector"),
     "StageController": ("probe_station_gui.stage_controller", "StageController"),
 }
 
